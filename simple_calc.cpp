cdfbfa8e0cb47fd2c6ac866529428aac2b379929
Vitor Lima <viitorlima2000@gmail.com>
2025-03-12 02:31:29 -0300

    Create simple_calc.cpp

---
+++ simple_calc.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int x, y;
+
+int main() {
+cout << "Type the first num: ";
+cin >> x;
+cout << "Type the second num: ";
+cin >> y;
+int sum = x + y;
+cout << "The sum of " << x << " and " << y << " is " << sum;
+return 0;}
