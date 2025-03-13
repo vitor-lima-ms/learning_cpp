22e5b9e928d093c79d30ad90aed5399b6dce9225
Vitor Lima <viitorlima2000@gmail.com>
2025-03-13 00:48:42 -0300

    Create main.cpp

---
+++ simple_sum/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int A;
+    int B;
+
+    std::cin >> A;
+    std::cin >> B;
+
+    int SOMA =  A + B;
+
+    std::cout << "SOMA = " << SOMA << "\n";
+
+    return 0;
+    }
