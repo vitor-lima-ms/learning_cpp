357fa15078d3214e6d498c0d45e88f10f0eeb7c7
Vitor Lima <viitorlima2000@gmail.com>
2025-03-13 00:48:51 -0300

    Create main.cpp

---
+++ simple_product/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int x;
+    int y;
+
+    std::cin >> x;
+    std::cin >> y;
+
+    int PROD = x * y;
+
+    std::cout << "PROD = " << PROD << "\n"
+    }
