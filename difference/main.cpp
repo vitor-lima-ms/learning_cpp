e7045bb0bdce8b6a9156861c5333b01f5a837caa
Vitor Lima <viitorlima2000@gmail.com>
2025-03-14 01:57:17 -0300

    Create difference

---
+++ difference/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A, B, C, D;
+    int diff;
+
+    cin >> A >> B >> C >> D;
+
+    diff = (A * B) - (C * D);
+
+    cout << "DIFERENCA = " << diff << "\n";
+
+    return 0;
+    }
